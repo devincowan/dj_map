c0be5c45fecd4ca4ad7cc4233f02e552d832612c
devincowan <dcquickdraw@gmail.com>
2020-06-24 17:03:01 -0600

    add date updated

--- dj_map_app/models.py
+++ dj_map_app/models.py
@@ -10,6 +10,7 @@ class Point(models.Model):
     """A Geo Point"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     geom = models.PointField('longitude/latitude', blank=True, null=True)
     objects = GeoManager()
