8f72bc5b01597d77d939fe659566161920396c07
devincowan <dcquickdraw@gmail.com>
2020-06-27 08:08:26 -0600

    small admin page change for points

--- dj_map_app/admin.py
+++ dj_map_app/admin.py
@@ -6,4 +6,9 @@ from leaflet.admin import LeafletGeoAdmin
 
 from .models import Point
 
-admin.site.register(Point, LeafletGeoAdmin)
+# admin.site.register(Point, LeafletGeoAdmin)
+
+
+@admin.register(Point)
+class PointAdmin(LeafletGeoAdmin):
+    list_display = ('text', 'geom', 'owner', 'date_added', 'date_updated')
